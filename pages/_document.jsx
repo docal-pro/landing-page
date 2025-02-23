2fa50bfdb02b32fea4a3032ad9ee49b592879db9
sshmatrix <cryptosauron@protonmail.com>
2025-02-23 20:10:29 +0530

    Meta icon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,6 +10,12 @@ export default class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100;200;300;400;500;600;700&family=Inter:wght@100..900&display=swap"
                         rel="stylesheet"
                     />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        href={`${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/assets/logo-light.png`}
+                        sizes="96x96"
+                    />
                 </Head>
                 <body>
                     <Main />
